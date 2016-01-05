d95850ae056ad8956f53c83df80274bb24bcb087
axfelix <axfelix@gmail.com>
2016-01-05 10:53:29 -0800

    handing variables to the shell script was screwed up, oops

--- includes/import-database.sh
+++ includes/import-database.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # $1 = database dump
 # $2 = mysql connect command
+# $3 = generated username
+# $4 = generated password
 
 # Sanitize database name in case none is provided from export
+# This handles incomplete exports from mysqldump and PMA
 DBNAME="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)"
-sed -i 's/-- Database: (.*?)/CREATE DATABASE \1;\nUSE DATABASE \1;/g' $1
-if grep -q "CREATE DATABASE" $1; then :; else sed -i 's/-- MySQL dump/CREATE DATABASE `$(echo $DBNAME)`;\nUSE DATABASE `$(echo $DBNAME)`;\n-- MySQL dump/g' $1; fi
+if grep -q "-- Database:" $1; then sed -i 's/-- Database: (.*?)/CREATE DATABASE \1;\nUSE DATABASE \1;/g' $1; fi
+if grep -q "CREATE DATABASE" $1; then :; else sed -i "s/-- MySQL dump/CREATE DATABASE $DBNAME;\nUSE $DBNAME;\n-- MySQL dump/g" $1; fi
 
-echo "CREATE USER '$user' IDENTIFIED BY '$password'" | $2
-echo "REVOKE ALL PRIVILEGES ON *.* FROM '$user'@'%'; FLUSH PRIVILEGES" | $2
-echo "GRANT SELECT ON $database TO '$user'@'%'; FLUSH PRIVILEGES" | $2
+# Get the DB name now that there's definitely one in there
+DBNAME="$(grep -oP '(?<=CREATE DATABASE )\S+(?=;)' $1)"
+
+$2 < $1
+echo "CREATE USER $3 IDENTIFIED BY $4" | $2
+echo "REVOKE ALL PRIVILEGES ON *.* FROM $3@'%'; FLUSH PRIVILEGES" | $2
+echo "GRANT SELECT ON $database TO $3@'%'; FLUSH PRIVILEGES" | $2
+
+echo $DBNAME
