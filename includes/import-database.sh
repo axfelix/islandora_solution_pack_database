492b160aa6b2cf569e157a99f60da7f9a197ba2b
axfelix <axfelix@gmail.com>
2016-01-05 11:14:21 -0800

    should fix everything but the broken pipes

--- includes/import-database.sh
+++ includes/import-database.sh
@@ -7,7 +7,7 @@
 # Sanitize database name in case none is provided from export
 # This handles incomplete exports from mysqldump and PMA
 DBNAME="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)"
-if grep -q "-- Database:" $1; then sed -i 's/-- Database: (.*?)/CREATE DATABASE \1;\nUSE DATABASE \1;/g' $1; fi
+if grep -qe "-- Database:" $1; then sed -i 's/-- Database: (.*?)/CREATE DATABASE \1;\nUSE DATABASE \1;/g' $1; fi
 if grep -q "CREATE DATABASE" $1; then :; else sed -i "s/-- MySQL dump/CREATE DATABASE $DBNAME;\nUSE $DBNAME;\n-- MySQL dump/g" $1; fi
 
 # Get the DB name now that there's definitely one in there
