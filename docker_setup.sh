7b86b58cbfa2fb8e4976cfda3c01abb952f3e051
axfelix <axfelix@gmail.com>
2016-01-07 10:06:58 -0800

    forgot the plugins!

--- docker_setup.sh
+++ docker_setup.sh
@@ -6,6 +6,11 @@ sudo service docker restart
 newgrp docker
 docker build -t tutum/lamp github.com/tutumcloud/lamp
 docker run -d -p 8008:80 -p 33060:3306 tutum/lamp
+docker exec -i $(docker ps | grep 33060 | sed -e "s/\s.*//g") mkdir /plugins
 docker cp includes/adminer-4.2.1-mysql.php $(docker ps | grep 33060 | sed -e "s/\s.*//g"):/adminer-4.2.1-mysql.php
-#workaround for the above command on docker <1.8
+docker cp includes/plugins/plugin.php $(docker ps | grep 33060 | sed -e "s/\s.*//g"):/plugins/plugin.php
+docker cp includes/plugins/frames.php $(docker ps | grep 33060 | sed -e "s/\s.*//g"):/plugins/frames.php
+#workaround for the above cp commands on docker <1.8
 #docker exec -i $(docker ps | grep 33060 | sed -e "s/\s.*//g") sh -c 'cat > /adminer-4.2.1-mysql.php' < includes/adminer-4.2.1-mysql.php
+#docker exec -i $(docker ps | grep 33060 | sed -e "s/\s.*//g") sh -c 'cat > /plugins/plugin.php' < includes/plugins/plugin.php
+#docker exec -i $(docker ps | grep 33060 | sed -e "s/\s.*//g") sh -c 'cat > /plugins/frames.php' < includes/plugins/frames.php
